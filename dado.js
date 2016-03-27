9f023231932d7bfb92dfe232b064b0f101602f93
angel morales <afmora_81@hotmail.com>
2016-03-27 12:31:03 -0500

    datos 3 veces

--- dado.js
+++ dado.js
@@ -1,4 +1,13 @@
-console.log("inicio del programa");
-var dado =Math.floor(Math.random() * 6) + 1;
-alert("dato dice:" + dado);
-console.log("programa completado");
+//console.log("inicio del programa");
+//var dado =Math.floor(Math.random() * 6) + 1;
+//alert("dato dice:" + dado);
+//console.log("programa completado");
+
+function tiraDados(){
+	var dado = Math.floor(Math.random() * 6) + 1;
+	alert(dado);
+}
+alert("tiramos 3 datos:");
+tiraDados();
+tiraDados();
+tiraDados();
---
+++ numero.js
@@ -0,0 +1,43 @@
+console.log("inicio del programa");
+var numerodeUsuario = prompt("ingrese un numero de1 al 5");
+var numeroAdivinar = 3;
+
+if(parseInt(numerodeUsuario)=== numeroAdivinar)
+{
+	adivinoCorrectamente =true;
+}
+	else if (parseInt(numerodeUsuario) > numeroAdivinar)
+	{
+		alert("el numero que ingreso es mayor");
+		var nuevoIntento =prompt("intenta den uevo un numero de 1 al 5:");
+		if (parseInt(nuevoIntento) === numeroAdivinar)
+	{
+		adivinoCorrectamente = true;
+	}
+	
+	else 
+	{
+		adivinoCorrectamente =false;
+	}
+}
+else if(parseInt(numerodeUsuario)< numeroAdivinar)
+{
+		alert("El numero a divinar es menor");
+		var nuevoIntento = prompt("intente de nuevo ,ingrese numero del 1 al 5:");
+		if(parseInt(nuevoIntento)=== numeroAdivinar)
+		{
+		adivinoCorrectamente =true;
+	}
+	else {
+		adivinoCorrectamente =false;	
+	}
+}
+	if (adivinoCorrectamente)
+	{
+		document.write ("<p> adivinaste</p>");
+	else {
+	document.write ("<p> no adivinaste</p>");
+	}
+	console.log("programa completado");
+
+
